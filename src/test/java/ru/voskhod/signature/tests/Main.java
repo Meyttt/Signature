1503999db2ffc8c999541feff99f0e329c0ae31e
a.chebotareva <sabermeyt@gmail.com>
2017-03-15 17:26:37 +0300

    Переход на страницу скачивания

--- src/test/java/ru/voskhod/signature/tests/Main.java
+++ src/test/java/ru/voskhod/signature/tests/Main.java
@@ -1,7 +1,6 @@
 package ru.voskhod.signature.tests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
@@ -10,7 +9,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
-import java.util.Scanner;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -21,25 +21,42 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
         capabilities.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR,"dismiss");
+        capabilities.setCapability(InternetExplorerDriver.INITIAL_BROWSER_URL,url);
         capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
         System.setProperty("webdriver.ie.driver",(new File("data/IEDriverServer.exe")).getAbsolutePath());
         InternetExplorerDriver driver = new InternetExplorerDriver(capabilities);
-        WebDriverWait wait = new WebDriverWait(driver,5);
-        driver.navigate().to(url);
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        while(!driver.getCurrentUrl().equalsIgnoreCase(url)){
-//            driver.close();
-//            driver = new InternetExplorerDriver(capabilities);
-//            driver.navigate().to(url);
-//            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        }
-//        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"colTwo\"]/h2")));
-        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        try {
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.id("cph2_listFileAccessCodes")));
+        }catch (org.openqa.selenium.TimeoutException e){
+            System.err.println("Драйвер запустился некорректно.");
+            driver.close();
+            driver.quit();
+            return;
+        }
+        String oldHandle = driver.getWindowHandle();
         driver.findElement(By.name("ctl00$cph2$fileUpload")).sendKeys(new File("data/cat2.JPG").getAbsolutePath());
         driver.findElement(By.name("ctl00$cph2$buttonFileUpload")).click();
-        Thread.sleep(5000);
-//        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"cph2_listFileAccessCodes\"]/option[1]")));
         driver.findElement(By.id("cph2_signDocuments")).click();
-
+        driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
+        Set<String> windows= driver.getWindowHandles();
+        windows.remove(oldHandle);
+        driver.switchTo().window(windows.toArray()[0].toString());
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("image-container")));
+        driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/div[1]/div[1]/a[2]")).click();
+        driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS);
+        driver.findElement(By.id("certificates-list")).findElements(By.xpath(".//*")).get(2).click();
+        driver.findElement(By.xpath("//*[@id=\"sign-document-dialog\"]/div/div/div/a[2]")).click();
+        driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS);
+        driver.findElement(By.id("pinCode")).sendKeys("123456");
+        List<WebElement> buttons = driver.findElements(By.className("ui-button-text"));
+        for(WebElement button:buttons){
+            if(button.getText().equalsIgnoreCase("Готово")){
+                button.findElement(By.xpath(".//..")).click();
+            }
+        }
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("cph2_ButtonDownload")));
+        driver.findElement(By.id("cph2_ButtonDownload"));
     }
 }
